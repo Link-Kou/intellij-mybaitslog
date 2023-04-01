cbfd35e516136d4cebf456f6aa661f6ab59ab0c3
likuo011@deppon.com <likuo011@deppon.com>
2023-04-01 10:50:37 +0800

    feat: 5.0.0发布

--- mybatis-idea-plugin/src/main/java/com/plugins/mybaitslog/PluginUtil.java
+++ mybatis-idea-plugin/src/main/java/com/plugins/mybaitslog/PluginUtil.java
@@ -1,8 +1,15 @@
 package com.plugins.mybaitslog;
 
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManagerCore;
+import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.extensions.PluginId;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.net.URL;
 
 public class PluginUtil {
 
@@ -21,25 +28,18 @@ public class PluginUtil {
      * @return String
      */
     private static String getJarPathByStartWith() {
-        //向下兼容
-        String defaultBaseDir = System.getProperty("java.io.tmpdir");
-        String d = defaultBaseDir + "mybatis-agent-1.0.18-all.jar";
-        File file = new File(d);
-        if (file.exists()) {
-            return d;
-        }
-        int length;
-        byte[] b = new byte[1024];
-        try (FileOutputStream fos = new FileOutputStream(d)) {
-            try (InputStream resourceAsStream = PluginUtil.class.getResourceAsStream("/mybatis-agent-1.0.18-all.jar");) {
-                while ((length = resourceAsStream.read(b)) != -1) {
-                    fos.write(b, 0, length);// 写入数据
-                    fos.flush();
+        PluginId pluginId = PluginId.getId("com.linkkou.plugin.intellij.assistant.mybaitslog");
+        final File filePlugin = PluginManagerCore.getPlugin(pluginId).getPluginPath().toFile();
+        final File[] fileslibs = filePlugin.listFiles();
+        for (File listFile : fileslibs) {
+            if ("lib".equals(listFile.getName())) {
+                final File[] fileslib = listFile.listFiles();
+                for (File file : fileslib) {
+                    if ("mybatis-agent-1.0.18-all.jar".equals(file.getName())) {
+                        return file.toPath().toString();
+                    }
                 }
             }
-            return d;
-        } catch (Exception e) {
-            e.printStackTrace();
         }
         return null;
     }
