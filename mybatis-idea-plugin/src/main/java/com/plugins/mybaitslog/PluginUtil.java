e574080ec69a0ba75c4fe2952a807d33a460fa82
likuo011@deppon.com <likuo011@deppon.com>
2023-04-13 17:53:13 +0800

    feat: 5.0.7 预发发布 feat: 新增错误提示处理。

--- mybatis-idea-plugin/src/main/java/com/plugins/mybaitslog/PluginUtil.java
+++ mybatis-idea-plugin/src/main/java/com/plugins/mybaitslog/PluginUtil.java
@@ -42,7 +42,7 @@ public class PluginUtil {
             if ("lib".equals(listFile.getName())) {
                 final File[] fileslib = listFile.listFiles();
                 for (File file : fileslib) {
-                    if ("mybatis-agent-1.0.23-all.jar".equals(file.getName())) {
+                    if ("mybatis-agent-1.0.24-all.jar".equals(file.getName())) {
                         return file.toPath().toString();
                     }
                 }
